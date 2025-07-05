2b026af9646843756a414bdd27c057fbe94d6364
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2025-07-05 17:22:34 +0900

    add figures and codes

---
+++ copy_fig.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cp codes/Fig00100/main.pdf fig_01.pdf
+cp codes/Fig00200/main.pdf fig_02.pdf
+cp codes/Fig00300/main.pdf fig_03.pdf
+
+cp codes/Fig00400/L16g2.0_D8_fig/fig_ene_errors_vs_rank.pdf fig_05a.pdf
+cp codes/Fig00400/L16g2.0_D8_fig/fig_ene_errors_vs_rank_log.pdf fig_05b.pdf
+cp codes/Fig00400/L16g2.0_D8_fig/fig_fidelity_errors_vs_steps.pdf fig_04.pdf
+
+cp codes/Fig00400/L16g0.5_D8_fig/fig_ene_errors_vs_rank.pdf fig_07a.pdf
+cp codes/Fig00400/L16g0.5_D8_fig/fig_ene_errors_vs_rank_log.pdf fig_07b.pdf
+cp codes/Fig00400/L16g0.5_D8_fig/fig_fidelity_errors_vs_steps.pdf fig_06.pdf
+
+cp codes/Fig00400/L16g1.0_D8_fig/fig_ene_errors_vs_rank.pdf fig_09a.pdf
+cp codes/Fig00400/L16g1.0_D8_fig/fig_ene_errors_vs_rank_log.pdf fig_09b.pdf
+cp codes/Fig00400/L16g1.0_D8_fig/fig_fidelity_errors_vs_steps.pdf fig_08.pdf
+
+cp codes/Fig01000/fig_optimization_open/fig_optimization_open.pdf fig_10.pdf
+cp codes/Fig01000/fig_error_vs_rank/fig_ene_errors_vs_rank.pdf fig_11a.pdf
+cp codes/Fig01000/fig_error_vs_rank/fig_ene_errors_vs_rank_log.pdf fig_11b.pdf
+cp codes/Fig01000/fig_optimization_periodic/fig_optimization_periodic.pdf fig_12.pdf
+
+cp codes/Fig01300/fig/fig.pdf fig_13.pdf
